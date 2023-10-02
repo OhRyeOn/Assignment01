f6695ce9f8e16a254ecd035021ea36b02a49a62c
Orion <Orion@Email.com>
2023-10-02 00:20:36 -0700

    Step 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-  // STEP 1
-  let someMonth
-  function theMonth()
-  let currentMonth
-  let summerMonth
-  let myLibraryFunction
+// STEP 1
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
 
+//Step 2
+numeric literal = 3.9
+string literal = 'Howdy'
+Boolean literal = false
+null literal = null
